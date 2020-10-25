7ff5a982731de95d8d911530d82b48d47a4056d8
Adri Torres <adriana.amaral@msn.com>
2020-10-25 03:15:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -40,7 +40,7 @@ end
 
 def retrieve_first_element_from_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
- index_number = 2
+  print array[0]
 end
 
 def retrieve_last_element_from_array(array)
