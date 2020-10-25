773ff3347d492caa772e033efe96e891011895dd
Adri Torres <adriana.amaral@msn.com>
2020-10-25 02:43:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,34 @@
-def create_an_empty_array
+def create_an_empty_array 
+  []
   
 end
 
-def create_an_array
+def create_an_array 
+  ["adri", "libby", "calla", "kiara"]
   
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array, element) 
+  array = ["wow", "I", "am", "really", "learning"]
+  element = "arrays!"
+  array << "arrays!"
+ 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array = ["I", "am", "really", "learning"]
+    element = "wow"
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+array = ["I", "am", "really", "learning", "arrays!"]
+   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -37,3 +46,4 @@ end
 def update_element_from_index(array, index_number, element)
 
 end
+
